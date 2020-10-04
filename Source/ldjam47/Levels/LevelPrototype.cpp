d33a8c2567eac0817972e715acc1368dccc873e9
Patrik <patrikfs5@gmail.com>
2020-10-04 17:34:56 +0200

    Fixed level collision

--- Source/ldjam47/Levels/LevelPrototype.cpp
+++ Source/ldjam47/Levels/LevelPrototype.cpp
@@ -1,18 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 /*----------------------------------------------------------------------------------------------------*/
+#include "LevelPrototype.h"
+
 #include "Components/BoxComponent.h"
 #include "Components/SceneComponent.h"
-#include "LevelPrototype.h"
 #include "PaperSpriteComponent.h"
+#include "Engine/CollisionProfile.h"
 /*----------------------------------------------------------------------------------------------------*/
 ALevelPrototype::ALevelPrototype()
 {
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 
 	_background = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Background"));
+	_background->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 	_background->SetupAttachment(RootComponent);
 
 	_foreground = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Foreground"));
+	_foreground->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 	_foreground->SetupAttachment(RootComponent);
 
 	_boxLeft = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Left"));
